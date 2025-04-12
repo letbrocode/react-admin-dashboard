194332663b40b9f041b6aab77187752ec25fd570
Pratik Kamble <pratikworkadd@gmail.com>
2025-04-12 23:41:52 +0530

    Orders page added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import ProductPage from "./pages/ProductPage";
 import Sidebar from "./components/Sidebar";
 import UsersPage from "./pages/UsersPage";
 import SalesPage from "./pages/SalesPage";
+import OrdersPage from "./pages/OrdersPage";
+import AnalyticsPage from "./pages/AnalyticsPage";
+import SettingsPage from "./pages/SettingsPage";
 
 function App() {
   return (
@@ -20,6 +23,9 @@ function App() {
         <Route path="/products" element={<ProductPage />} />
         <Route path="/users" element={<UsersPage />} />
         <Route path="/sales" element={<SalesPage />} />
+        <Route path="/orders" element={<OrdersPage />} />
+        <Route path="/analytics" element={<AnalyticsPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
       </Routes>
     </div>
   );
